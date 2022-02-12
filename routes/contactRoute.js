cbc6fcb9784f0e4cb7139adf07754c56abd26b22
Kagisomphayi <mphayiw@gmail.com>
2022-02-12 12:03:35 +0200

    make it better

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -10,10 +10,10 @@ app.post("/", (req, res) => {
 
   console.log(process.env.EMAIL, process.env.PASS)
   var transporter = nodemailer.createTransport({
-    service: "gmail",
+    // service: "gmail",
     host: "smtp.gmail.com",
     port: 465,
-    secure: true,
+    // secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASS,
@@ -22,10 +22,16 @@ app.post("/", (req, res) => {
 
   var mailOptions = {
     from: email,
-    to: "mphayi96@gmail.com",
+    to: `${process.env.EMAIL}`,
     subject: `${subject}`,
     text: `${name} has messaged you saying ${message}`,
   };
+
+  transporter.verify((error, success) => {
+    if (error) {
+      console.log(error)
+    }
+  });
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
